16955e0ad7fb57b2cdd506582902bf8807a432c9
christine gomez <christine.gomez1226@gmail.com>
2018-09-13 15:52:28 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -2,7 +2,7 @@ def nyc_pigeon_organizer(data)
   new_hash ={}
   data.each do |info, values|
     values.each do |value, name|
-      name.each do |pigeon| 
+      name.each do |pigeon|
         new_hash[pigeon] || = {}
         new_hash[pigeon][info] << value.to_s
       end
